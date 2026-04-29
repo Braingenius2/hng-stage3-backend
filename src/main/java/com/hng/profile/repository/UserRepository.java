ff8085131422746a682d85e65e2503dd5c5f5349
Braingenius2 <braingenius2@gmail.com>
2026-04-29 13:23:01 +0100

    feat: implement security configuration and user repository for authentication setup

---
+++ src/main/java/com/hng/profile/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.hng.profile.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.hng.profile.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+  Optional<User> findByGithubId(String githubId);
+}
